72966bea25d6d951404816b5242b11405fd85488
Robert Ungureanu <robertgamingclash@gmail.com>
2024-06-10 20:12:53 +0300

    Added shadcn/ui and environment variables

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,10 +3,13 @@ import { type Config } from "drizzle-kit";
 import { env } from "@/env";
 
 export default {
-  schema: "./src/server/db/schema.ts",
-  dialect: "mysql",
-  dbCredentials: {
-    url: env.DATABASE_URL,
-  },
-  tablesFilter: ["hintermann-webapp_*"],
+    schema: "./src/server/db/schema.ts",
+    dialect: "mysql",
+    dbCredentials: {
+        host: env.DB_HOST,
+        user: env.DB_USER,
+        password: env.DB_PASSWORD,
+        database: env.DB_NAME,
+    },
+    tablesFilter: ["hintermann-webapp_*"],
 } satisfies Config;
