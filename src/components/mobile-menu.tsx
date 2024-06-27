179b009c72f7a7a77ecd0854fec511b3fc8c3419
Robert Ungureanu <robertgamingclash@gmail.com>
2024-06-27 20:51:08 +0300

    fixed the mobile menu button on the desktop

--- src/components/mobile-menu.tsx
+++ src/components/mobile-menu.tsx
@@ -21,7 +21,7 @@ export function HeaderMobileMenu({ email }: { email?: string }) {
     ];
     return (
         <MobileMenu>
-            <MobileMenuTrigger className="flex h-10 w-10 items-center justify-center rounded-md border">
+            <MobileMenuTrigger className="flex h-10 w-10 items-center justify-center rounded-md border lg:hidden">
                 <Menu />
             </MobileMenuTrigger>
             <MobileMenuContent>
